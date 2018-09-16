97fd5e1011e50f72feeb210296bd645d3c19cc49
thomasmartin86 <thomas.martin1126@gmail.com>
2018-09-15 22:47:23 -0400

    first commit

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+/* keys removed for privacy on github */
+module.exports = {
+  mongoURI: '',
+  pusherAppId: '',
+  pusherKey: '',
+  pusherSecret: '',
+  pusherCluster: '',
+  pusherEncrypted: true
+};
